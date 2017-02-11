acfd79a6e7edcfa70a6ae1f8add441826f75a86a
Sagar Routh <Sagar Routh>
2017-02-11 18:33:14 +0530

    Refactor server.js

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
 const express 	 = 	require('express');
 const fs 		 = 	require('fs');
 const bodyParser = 	require('body-parser');
+const path 		 =	require('path');
 const app 		 = 	express();
+
 
 var port = process.env.PORT || 3000;
 
 
 //app level middleware
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //load models into app
-fs.readdir('./models', (err, models)=>{
-	if(err)
-		console.log(err);
-	else {
-		models.forEach(function(model){
-			require('./models'+'/'+model);
-		});
-	}
+fs.readdirSync('./models').forEach((model) => {
+	if(path.extname(model)==='.js')
+		require(path.join(__dirname, 'models', model));
 });
 
+
 //load controllers into app
-fs.readdir('./controllers', (err, controllers)=>{
-	if(err)
-		console.log(err);
-	else {
-		controllers.forEach(function(controller){
-			var route = require('./controllers'+'/'+controller);
-			route.controller(app);
-		});
+fs.readdirSync('./controllers').forEach((controller)=>{
+	if(path.extname(controller)==='.js'){
+		var route = require(path.join(__dirname, 'controllers', controller));
+		route.controller(app);
 	}
 });
+	
 
 app.listen(port, ()=>{
 	console.log('server started');
